654f3277ec6eff1ac80b38bc12d456260482bdd5
franosores <francisca@montblanc.ai>
2025-02-19 11:46:38 -0300

    [UPDATE] integrate order management components and update MenuItem to handle item addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import {menuItems} from "./data/db.ts";
+import useOrder  from "./hooks/useOrder.ts";
 import MenuItem from "./components/MenuItem.tsx";
-function App() {
+import OrderContent from "./components/OrderContent.tsx";
+import OrderTotals from "./components/OrderTotals.tsx";
+import TipPercentageForm from "./components/TipPercentageForm.tsx";
 
+function App() {
+    const { order, addItem, removeItem } = useOrder();
   return (
     <>
         <header className="bg-teal-400 py-5">
@@ -10,16 +15,30 @@ function App() {
 
         <main className="max-w-7xl mx-auto py-20 grid  md:grid-cols-2">
             <div className="p-5">
-                <h2 className="test-4xl font-black">Menú</h2>
+                <h2 className="text-4xl font-black">Menú</h2>
                 <div className="space-y-4">
                 {menuItems.map((item) => (
-                    <MenuItem key={item.id} item={item} />
+                    <MenuItem
+                        key={item.id}
+                        item={item}
+                        addItem={addItem}
+                    />
                     ))
                 }
                 </div>
             </div>
-           <div>
-               <h2>Consumo</h2>
+           <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+               <OrderContent
+                   key={order.id}
+                   order={order}
+                   removeItem={removeItem}
+               />
+               <TipPercentageForm
+
+               />
+                <OrderTotals
+                     order={order}
+                />
            </div>
 
 
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,12 +2,14 @@ import type { MenuItem } from "../types";
 
 type MenuItemProps = {
     item: MenuItem;
+    addItem: (item: MenuItem) => void;
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({item, addItem} : MenuItemProps) {
     return (
         <button
         className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+        onClick={() => addItem(item)}
         >
         <p>
             {item.name}
